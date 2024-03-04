587743dbf38620b9a74d723cb36ed0932ffc0f14
Boeun's <rnqhdms@gmail.com>
2024-03-04 18:49:27 +0900

    다수의 Pointcut과 advice를 사용하고 싶다면

--- aop/src/main/java/hello/aop/order/aop/AspectV3.java
+++ aop/src/main/java/hello/aop/order/aop/AspectV3.java
@@ -6,17 +6,22 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+/**
+ * 다른 Pointcut을 각각의 advice에 적용하고 싶다면?
+ */
 @Slf4j
 @Aspect
 public class AspectV3 {
 
     //hello.aop.order 패키지와 하위 패키지
     @Pointcut("execution(* hello.aop.order..*(..))")
-    private void allOrder(){} //pointcut signature
+    private void allOrder() {
+    } //pointcut signature
 
     //클래스 이름 패턴이 *Service
     @Pointcut("execution(* *..*Service.*(..))")
-    private void allService(){}
+    private void allService() {
+    }
 
     @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
