ef05f4b6b0568d8373a766a9b2dd47e052b1af02
Boeun's <rnqhdms@gmail.com>
2024-03-05 00:37:46 +0900

    pointcut 표현식 정리 (PCD)  : annotation

--- aop/src/test/java/hello/aop/pointcut/AtAnnotationTest.java
+++ aop/src/test/java/hello/aop/pointcut/AtAnnotationTest.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+/**
+ * 메서드에 주어진 애노테이션을 조인포인트 적용 판단 기준으로 함
+ */
 @Slf4j
 @Import(AtAnnotationTest.AtAnnotationAspect.class)
 @SpringBootTest
@@ -30,7 +33,7 @@ public class AtAnnotationTest {
 
         @Around("@annotation(hello.aop.member.annotation.MethodAop)")
         public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
-            log.info("[@annotation] {}", joinPoint.getSignature());
+            log.info("프록시의 부가 기능 실행! => [@annotation] {}", joinPoint.getSignature());
             return joinPoint.proceed();
         }
 
