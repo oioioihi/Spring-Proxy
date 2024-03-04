b1c07e9a415f7fbda4e8646487c4e707b6edceaa
Boeun's <rnqhdms@gmail.com>
2024-03-04 19:18:23 +0900

    Advice의 종류

--- aop/src/main/java/hello/aop/order/aop/AspectV6Advice.java
+++ aop/src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -5,6 +5,15 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
+/**
+ * Advice의 종류
+ *
+ * @Around : 메서드 호출 전후에 수행 된다. 가장 강력한 advice로 join point 실행 여부 선택, 반환 값 변환, 예외 변환 등이 가능하다.
+ * @Before : join point 실행 이전에 실행 된다.
+ * @AfterReturning : join point가 정상 완료 후 실행 된다.
+ * @AfterThrowing : 메서드가 예외를 던지는 경우에 실행 된다.
+ * @After : join point가 정상 또는 예외발생에 관계없이 실행된다. (finally)
+ */
 @Slf4j
 @Aspect
 public class AspectV6Advice {
@@ -14,39 +23,39 @@ public class AspectV6Advice {
 
         try {
             //@Before
-            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            log.info("1 : [트랜잭션 시작] {}", joinPoint.getSignature());
             Object result = joinPoint.proceed();
             //@AfterReturning
-            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            log.info("5 : [트랜잭션 커밋] {}", joinPoint.getSignature());
             return result;
         } catch (Exception e) {
             //@AfterThrowing
-            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            log.info("7 : [트랜잭션 롤백] {}", joinPoint.getSignature());
             throw e;
         } finally {
             //@After
-            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+            log.info("8 : [리소스 릴리즈] {}", joinPoint.getSignature());
         }
     }
 
     @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
     public void doBefore(JoinPoint joinPoint) {
-        log.info("[before] {}", joinPoint.getSignature());
+        log.info("2 : [before] {}", joinPoint.getSignature());
     }
 
     @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
     public void doReturn(JoinPoint joinPoint, Object result) {
-        log.info("[return] {} return={}", joinPoint.getSignature(), result);
+        log.info("3 : [return] {} return={}", joinPoint.getSignature(), result);
     }
 
     @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
     public void doThrowing(JoinPoint joinPoint, Exception ex) {
-        log.info("[ex] {} message={}", ex);
+        log.info("6 : [ex] {} message={}", ex);
     }
 
     @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
     public void doAfter(JoinPoint joinPoint) {
-        log.info("[after] {}", joinPoint.getSignature());
+        log.info("4 : [after] {}", joinPoint.getSignature());
     }
 
 }
